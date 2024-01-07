c1e9df9544c6a52373de6d5495729ad8716dc3c9
prafullpandey614 <prafullpandey614@gmail.com>
2024-01-07 18:09:25 +0530

    test cases for all-uploaded-files api

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,35 +7,30 @@ from django.contrib.auth.models import User
 class TestGetDownloadLinkAPIView:
     def setup_method(self, method):
         self.client = APIClient()
-
+        self.user = User.objects.create_user(username='testuser',email="tese@gmail.com", password='testpassword')
+        
     def test_get_download_link(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(path="/api/all-files")
 
-        response = self.client.get(path="/api/down/1")
 
         assert response.status_code == status.HTTP_200_OK
-        assert 'api/download/1' in response.data
+    def test_get_download_un_authorised(self):
+    
+        response = self.client.get(path="/api/all-files")
 
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    def test_get_download_wrong_method(self):
+        
+        response = self.client.get(path="/api/all-files")
+
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
     def test_get_download_link_invalid_pk(self):
 
         response = self.client.get(path="/api/down/930")
 
         assert response.status_code == status.HTTP_404_NOT_FOUND
 
-@pytest.mark.django_db
-class TestOperationUser:
-    def setup_method(self, method):
-        self.client = APIClient()
-        self.user = User.objects.create_user(username='testuser',email="tese@gmail.com", password='testpassword')
-        self.client.force_authenticate(user=self.user)
-    def test_op_user(self):
 
-        response = self.client.post(path="/api/token/",data={"username" :"testuser","Password":"testpassword"})
-
-        assert response.status_code == status.HTTP_200_OK
-
-
-    def test_get_download_link_invalid_pk(self):
-
-        response = self.client.get(path="/api/down/10231")
-
-        assert response.status_code == status.HTTP_404_NOT_FOUND
